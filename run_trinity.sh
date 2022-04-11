d567fb1791023a29701328a10896d04e2b267d40
Argonarx <armando12123434@gmail.com>
2022-04-11 18:53:53 -0300

    version official 2.0

--- run_trinity.sh
+++ run_trinity.sh
@@ -6,6 +6,3 @@ basedir=`readlink -e $0 | sed 's:\/[^\/]*$::g'`
 echo '--> Iniciando el servidor (trinity)'
 bash $basedir/server/run_server.sh &> /dev/null &
 
-echo '--> Iniciando el cliente (trinity)'
-bash $basedir/client/python_01/run_pyclient.sh &> /dev/null &
-
--- stop_trinity.sh
+++ stop_trinity.sh
@@ -5,7 +5,3 @@ basedir=`readlink -e $0 | sed 's:\/[^\/]*$::g'`
 
 echo '--> Deteniendo el servidor (trinity)'
 bash $basedir/server/stop_server.sh &> /dev/null &
-
-echo '--> Deteniendo el cliente (trinity)'
-bash $basedir/client/python_01/stop_pyclient.sh &> /dev/null &
-
