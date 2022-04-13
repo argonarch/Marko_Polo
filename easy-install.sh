27567ef4bee73a1c5fd9e2cb1f90011c65f915d8
Argonarx <51279889+argonarch@users.noreply.github.com>
2022-04-13 13:29:03 -0300

    Update easy-install.sh

--- easy-install.sh
+++ easy-install.sh
@@ -2,8 +2,9 @@
 
 cd $HOME
 
+sudo apt install python3-pip git python3-pyaudio libatlas-base-dev screen
+
 git clone https://github.com/argonarch/Marko_Polo.git
 
 pip install paho-mqtt SpeechRecognition pvporcupine pvrecorder
 
-sudo apt install python3-pyaudio libatlas-base-dev screen
