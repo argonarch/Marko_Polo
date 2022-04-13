666bc17ac70a5e54d7daa840e85c2c8b96a0afc6
Argonarx <armando12123434@gmail.com>
2022-04-12 22:50:04 -0300

    agregado de dependencias en el instalador

--- easy-install.sh
+++ easy-install.sh
@@ -4,6 +4,6 @@ cd $HOME
 
 git clone https://github.com/argonarch/trinity.git
 
-pip install paho-mqtt SpeechRecognition
+pip install paho-mqtt SpeechRecognition pvporcupine pvrecorder
 
 sudo apt install python3-pyaudio libatlas-base-dev screen
